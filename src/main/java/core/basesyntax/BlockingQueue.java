c381d7c81f88e68b97f1a9281543d2fdda685e9d
Olha Starykova <Ole4kastar79@gmail.com>
2026-01-04 18:48:41 +0200

    BlockingQueue realized

--- src/main/java/core/basesyntax/BlockingQueue.java
+++ src/main/java/core/basesyntax/BlockingQueue.java
@@ -12,16 +12,31 @@ public class BlockingQueue<T> {
     }
 
     public synchronized void put(T element) throws InterruptedException {
-        // write your code here
+        while (queue.size() >= this.capacity) {
+            System.out.println("Queue is overload");
+            this.wait();
+        }
+        queue.add(element);
+        this.notifyAll();
+        System.out.println("Added element " + element);
+
     }
 
     public synchronized T take() throws InterruptedException {
-        // write your code here
-        return null;
+        try {
+            while (this.isEmpty()) {
+                this.wait();
+            }
+            T pooled = queue.poll();
+            this.notifyAll();
+            return pooled;
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public synchronized boolean isEmpty() {
         // write your code here
-        return true;
+        return queue.isEmpty();
     }
 }
